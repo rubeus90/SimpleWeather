6b4c4037a73bb09bfcb096d1bd64c46c1d885192
Hong Ngoc Nguyen <rubeus90@gmail.com>
2020-01-21 09:58:53 -0500

    Initiate an empty Android project

---
+++ app/src/main/java/com/rubeus/simpleweather/WeatherActivity.kt
@@ -0,0 +1,12 @@
+package com.rubeus.simpleweather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WeatherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+    }
+}
